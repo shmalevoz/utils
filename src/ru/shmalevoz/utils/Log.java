66e77a7af77045d20c47a5f12691f91ba82e9d21
Valeriy Krynin <shmalevoz@gmail.com>
2013-08-03 20:55:37 +0600

    Добавлены javadoc комментарии. Небольшие правки

--- src/ru/shmalevoz/utils/Log.java
+++ src/ru/shmalevoz/utils/Log.java
@@ -10,7 +10,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * Сервисная обертка над java.util.Logging
  * @author shmalevoz
  */
 public class Log {
@@ -18,7 +18,12 @@ public class Log {
 	public static int LEVEL_DEFAULT = 2;
 	public static final int LEVEL_CONFIG = 7;
 	public static final int LEVEL_VERBOSE_DEFAULT = 3;
+	public static final int LEVEL_WARNING = 2;
 	
+	/**
+	 * Устанавливает уровень логгирования по-умолчанию
+	 * @param level Новый уровень
+	 */
 	public static void setDefaultLevel(String level) {
 		int l = LEVEL_DEFAULT;
 		try {
@@ -28,24 +33,48 @@ public class Log {
 		setDefaultLevel(l);
 	}
 	
+	/**
+	 * Устанавливает уровень логгирования по-умолчанию
+	 * @param level Новый уровень
+	 */
 	public static void setDefaultLevel(int level) {
 		LEVEL_DEFAULT = level;
 	}
 	
+	/**
+	 * Возвращает именованный поток лога
+	 * @param name Имя потока
+	 * @return Объект лога
+	 */
 	public static Logger getLogger(String name) {
 		return getLogger(name, LEVEL_DEFAULT);
 	}
 	
+	/**
+	 * Возвращает именованный поток лога
+	 * @param name Имя потока
+	 * @param level Назначаемый уровень логгирования
+	 * @return Объект лога
+	 */
 	public static Logger getLogger(String name, int level) {
 		Logger log = Logger.getLogger(name);
 		init(log, level);
 		return log;
 	}
 	
+	/**
+	 * Инициализируем объект лога
+	 * @param log Логгер
+	 */
 	public static void init(Logger log) {
 		init(log, LEVEL_DEFAULT);
 	}
 	
+	/**
+	 * Инициализирует объект лога
+	 * @param log Логгер
+	 * @param level Уровень логгирования
+	 */
 	public static void init(Logger log, int level) {
 		for (Handler handler:log.getParent().getHandlers()) {
 			log.getParent().removeHandler(handler);
@@ -56,10 +85,19 @@ public class Log {
 		setLevel(log, level);
 	}
 	
+	/**
+	 * Устанавливает логгеру уровень логгирования по-умолчанию
+	 * @param log Логгер
+	 */
 	public static void setLevel(java.util.logging.Logger log) {
 		setLevel(log, LEVEL_DEFAULT);
 	}
 	
+	/**
+	 * Устанавливает логгеру новый уровень логгирования
+	 * @param log Логгер
+	 * @param level Новый уровень
+	 */
 	public static void setLevel(java.util.logging.Logger log, String level) {
 		int l = LEVEL_DEFAULT;
 		try {
@@ -71,6 +109,11 @@ public class Log {
 		setLevel(log, l);
 	}
 	
+	/**
+	 * Устанавливает логгеру новый уровень логгирования
+	 * @param log Логгер
+	 * @param level Новый уровень
+	 */
 	public static void setLevel(java.util.logging.Logger log, int level) {
 		int l = level;
 		if (l < 0 || l > 8) {
